d6b33dc4e2e38ae8957b4074b391d86e0f8722d0
Rodrigo M.V.S <88489374+RodrigoMvs123@users.noreply.github.com>
2024-04-15 10:08:28 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const PORT = 8000 
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const OpenAI = require("openai")
+require('dotenv').config()
+
+app.use(express.json())
+app.use(cors())
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
+
+app.post('/completion', async (req, res) => {
+  const text = req.body.text
+  const completion = await openai.chat.completions.create({
+    messages: [{"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": text}
+      ],
+    model: "gpt-3.5-turbo",
+  });
+  console.log(completion)
+  // res.send(completion.choises[0])
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
